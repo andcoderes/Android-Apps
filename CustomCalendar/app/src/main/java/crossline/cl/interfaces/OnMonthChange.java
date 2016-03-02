4318e7b6e8b2206ed35aae60057d80770bfde1b3
Jaime Acevedo <jaim.acevedoc@gmail.com>
2016-03-01 21:50:36 -0300

    Remove Proyect

--- CustomCalendar/app/src/main/java/crossline/cl/interfaces/OnMonthChange.java
+++
@@ -1,9 +0,0 @@
-package crossline.cl.interfaces;
-
-/**
- * Created by jacevedo on 02-09-14.
- */
-public interface OnMonthChange
-{
-    public void onMonthChange(String previousMonth, String actualMonth);
-}
--- CustomImageCarousel/app/src/main/java/crossline/cl/interfaces/OnCarouselClickListener.java
+++
@@ -1,9 +0,0 @@
-package crossline.cl.interfaces;
-
-/**
- * Created by jacevedo on 06-09-14.
- */
-public interface OnCarouselClickListener
-{
-    public void onPictureClickListener();
-}
